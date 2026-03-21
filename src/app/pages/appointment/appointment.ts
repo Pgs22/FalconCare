8e94a389b6ecf4dc43dd3298370869c0cd4115e3
patricia <patricia.gsbcn@gmail.com>
2026-03-21 11:12:24 +0100

    #4 styles and delete model tempory

--- src/app/pages/appointment/appointment.ts
+++ src/app/pages/appointment/appointment.ts
@@ -18,55 +18,8 @@ export class AppointmentComponent implements OnInit {
 
   constructor(private readonly appointmentService: AppointmentService) {}
 
-  // ngOnInit(): void {
-  //   this.fetchAppointments();
-  // }
-
-  // fetchAppointments(): void {
-  //   this.error.set(null);
-  //   this.loading.set(true);
-
-  //   this.appointmentService.getAppointments().subscribe({
-  //     next: (data) => {
-  //       this.appointments.set(data);
-  //       this.loading.set(false);
-  //     },
-  //     error: (err) => {
-  //       this.error.set('No s’ha pogut connectar amb el servidor.');
-  //       this.loading.set(false);
-  //     }
-  //   });
-  // }
-
   ngOnInit(): void {
-    this.loading.set(true);
-
-    const mockData: Appointment[] = [
-      { 
-        id: 1, 
-        time: '09:00', 
-        duration: 60, 
-        status: 'confirmed', 
-        patientName: 'Amanda Smith', 
-        doctorName: 'Dra. Sarah Jenkins', 
-        box: 'BOX 1', 
-        reason: 'Ortodòncia' 
-      },
-      { 
-        id: 2, 
-        time: '12:00', 
-        duration: 90, 
-        status: 'pending', 
-        patientName: 'Eleanor Pena', 
-        doctorName: 'Dr. Michael Ross', 
-        box: 'BOX 2', 
-        reason: 'Sensibilitat molar' 
-      }
-    ];
-
-    this.appointments.set(mockData);
-    this.loading.set(false);
-
+    this.fetchAppointments();
   }
 
   fetchAppointments(): void {
@@ -84,4 +37,5 @@ export class AppointmentComponent implements OnInit {
       }
     });
   }
+
 }
